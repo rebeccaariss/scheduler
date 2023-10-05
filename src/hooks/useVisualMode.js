9861f17000c2a2e3a06d26acb31be4e4b53cb3f8
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-10-05 17:02:15 -0400

    Implement test for ability to go back to previous mode (and associated function)

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  // const [mode, setMode] = useState(initial);
+  // ⬆️ Mode is no longer needed because now we're tracking history
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setMode(newMode);
+  function transition(newMode){
+    setHistory(prev => [...prev, newMode])
   }
 
-  return { mode, transition };
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+  }
+
+  return { mode: history[history.length - 1], transition, back };
 }
