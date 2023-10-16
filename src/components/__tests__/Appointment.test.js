e15190db5c79b7d87c8ebb87403f71ad3c6149ec
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-10-16 12:52:37 -0400

    Add integration test for booking interview

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -36,4 +36,24 @@ describe("Application", () => {
   
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    console.log(prettyDOM(appointment));
+  });
 });
