facd354bca0d6ec02d9b8a2d660235b9b2f6ae3c
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-10-18 09:21:49 -0400

    Complete test for booking an interview (full cycle) (test is not working at this point, test db needs to be configured)

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -38,7 +38,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
   
     await waitForElement(() => getByText(container, "Archie Cohen"));
   
@@ -50,10 +50,24 @@ describe("Application", () => {
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
+
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-  
     fireEvent.click(getByText(appointment, "Save"));
+
+    // We should go from CREATE mode to SAVING, never directly to SHOW:
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    // This should cause the test to fail (something wrong in implementation - debug later):
+    // expect(getByText(appointment, "Saving")).not.toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
   
-    console.log(prettyDOM(appointment));
+    // console.log(prettyDOM(appointment));
+
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
