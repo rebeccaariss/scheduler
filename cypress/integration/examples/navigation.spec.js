e44af314400419c95b26eb80120c5a08156ac670
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-10 12:31:38 -0500

    Add initial tests for navigating days

--- cypress/integration/examples/navigation.spec.js
+++ cypress/integration/examples/navigation.spec.js
@@ -4,4 +4,16 @@ context('Navigation', () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.get("li")
+      .contains("Tuesday")
+      .click();
+    
+    cy.get("li")
+      .contains("li", "Tuesday")
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
 })
