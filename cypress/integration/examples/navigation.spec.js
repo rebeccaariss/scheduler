a0e7270e17a6d53833753a67a1d338e4ceeed3aa
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-10 12:32:52 -0500

    Refactor test for navigating days

--- cypress/integration/examples/navigation.spec.js
+++ cypress/integration/examples/navigation.spec.js
@@ -8,12 +8,8 @@ context('Navigation', () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
 
-    cy.get("li")
-      .contains("Tuesday")
-      .click();
-    
-    cy.get("li")
-      .contains("li", "Tuesday")
+    cy.contains("li", "Tuesday")
+      .click()
       .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 })
